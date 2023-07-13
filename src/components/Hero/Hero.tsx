d027a333ee431d0d5c864e59866fcefeae62f89e
RODRY <rodry_valenzuela@hotmail.com>
2023-07-13 12:36:15 -0300

    responsive 90% completo

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,6 +8,7 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
     const transition = {type: 'spring', duration: 3};
+    const mobile = window.innerWidth<=768 ? true: false;
     return (
         <div className="hero">
             <div className="blur hero-blur"></div>
@@ -16,7 +17,7 @@ const Hero = () => {
 
                 <div className="the-best-ad">
                     <motion.div
-                    initial = {{left: '238px'}}
+                    initial = {{left: mobile? '165px': '238px'}}
                     whileInView={{left: '8px'}}
                     transition={{...transition, type: 'tween'}}
                     ></motion.div>
@@ -63,7 +64,7 @@ const Hero = () => {
                 </div>
             </div>
             <div className="right-h">
-                <button className="btn">Unete Ahora</button>
+                
 
                 <motion.div
                     initial = {{right: "-1rem"}}
